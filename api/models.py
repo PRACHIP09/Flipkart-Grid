954098aef763fce4beaa2dcd9ad1eed94875996e
Jenil-Savla <you@example.com>
2022-07-15 13:49:05 +0530

    Models and Serializers created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 100)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+
+    class Meta:
+        verbose_name_plural = 'Sub-Categories'
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+def upload_path_handler(instance, filename):
+    return "images/products/{label}/{file}".format(
+        label=instance.name, file=filename
+    )
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank= True, null= True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    price = models.FloatField(default = 0.0)
+    discount = models.BooleanField(default = False)
+    offer_price = models.FloatField(default=0.0)
+    stock = models.PositiveIntegerField(default = 0)
+    description = models.CharField(max_length = 255, blank= True, null= True)
+    image = models.ImageField(upload_to = upload_path_handler,null = True, blank = True)
+
+    hastags = models.PositiveIntegerField(default = 0)
+    buyers = models.PositiveIntegerField(default = 0)
+    rating = models.PositiveIntegerField(default = 0)
+    searches = models.PositiveIntegerField(default = 0)
+    viewers = models.PositiveIntegerField(default = 0)
+
+    def __str__(self):
+        return self.name
+
+def upload_path_handler(instance, filename):
+    return "videos/{label}/{file}".format(
+        label=instance.name, file=filename
+    )
+
+class Video(models.Model):
+    name = models.CharField(max_length = 100)
+    file = models.FileField(upload_to = upload_path_handler,null = True, blank = True)
+    views = models.PositiveIntegerField(default = 0)
+    
+    def __str__(self):
+        return self.name
+
+def upload_path_handler(instance, filename):
+    return "images/blogs/{label}/{file}".format(
+        label=instance.title, file=filename
+    )
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to = upload_path_handler,null = True, blank = True)
+
+    def __str__(self):
+        return self.title
---
+++ api/serializers.py
@@ -0,0 +1,86 @@
+from rest_framework import serializers
+from .models import Category, SubCategory, Brand, Product, Video, Blog
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    subcategory = SubCategorySerializer()
+    brand = BrandSerializer()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def create(self, validated_data):
+        category_validated_data = validated_data.pop('category', None)
+        subcategory_validated_data = validated_data.pop('subcategory', None)
+        brand_validated_data = validated_data.pop('brand', None)
+
+        category = Category.objects.create(**category_validated_data)
+        category.save()
+
+        subcategory = SubCategory.objects.create(category=category, **subcategory_validated_data)
+        subcategory.save()
+
+
+        brand = Brand.objects.create(**brand_validated_data)
+        brand.save()
+
+        product = Product.objects.create(category=category, subcategory=subcategory, brand=brand, **validated_data)
+        return product
+
+    def update(self, instance, validated_data):
+        category_validated_data = validated_data.pop('category', None)
+        subcategory_validated_data = validated_data.pop('subcategory', None)
+        brand_validated_data = validated_data.pop('brand', None)
+
+        category = instance.category                   
+        category.name = category_validated_data.get('name', category.name)
+        category.save()
+
+        subcategory = instance.subcategory                  
+        subcategory.name = subcategory_validated_data.get('name', subcategory.name)
+        subcategory.save()
+
+        brand = instance.brand                  
+        brand.name = brand_validated_data.get('name', brand.name)
+        brand.save()
+
+        instance.name = validated_data.get('name', instance.name)
+        instance.price = validated_data.get('price', instance.price)
+        instance.discount = validated_data.get('discount', instance.discount)
+        instance.offer_price = validated_data.get('offer_price', instance.offer_price)
+        instance.stock = validated_data.get('stock', instance.stock)
+        instance.description = validated_data.get('description', instance.description)
+        instance.image = validated_data.get('image', instance.image)
+        instance.hastags = validated_data.get('hastags', instance.hastags)
+        instance.buyers = validated_data.get('buyers', instance.buyers)
+        instance.rating = validated_data.get('rating', instance.rating)
+        instance.searches = validated_data.get('searches', instance.searches)
+        instance.viewers = validated_data.get('viewers', instance.viewers)
+        instance.save()
+        return instance
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = '__all__'
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
