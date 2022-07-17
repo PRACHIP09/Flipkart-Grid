28107a10cebdae3479ddc635d8b7906848a4fa24
Jenil-Savla <you@example.com>
2022-07-17 17:38:20 +0530

    Added CORS

--- flipkart_grid/settings.py
+++ flipkart_grid/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['flipkart-grid.herokuapp.com','127.0.0.1']
 
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000"
+]
 
 # Application definition
 
@@ -40,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
     'api',
 
 ]
@@ -47,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
